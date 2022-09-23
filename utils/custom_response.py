8c2058079f181f4f7971964a1996a481ca34b2fc
Larshabi <lekanlasabi@gmail.com>
2022-09-23 01:16:14 +0100

    utils

--- utils/custom_response.py
+++ utils/custom_response.py
@@ -3,7 +3,7 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import PermissionDenied
-from core.pagination import StandardResultsSetPagination
+# from core.pagination import StandardResultsSetPagination
 
 
 
